dfba9d628245aa306ba3735dbbdacf20484c51b9
Jakub Gawlikowski <jgawlikowski@ecovadis.com>
2021-05-17 15:25:10 +0200

    adding reducer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { FeatureToggle } from './FeatureToggle';
 export { On } from './On';
 export { Off } from './Off';
+export { featureToggleReducer } from './reducer';
---
+++ src/reducer.js
@@ -0,0 +1,37 @@
+import {
+  CLEAR_FEATURE_TOGGLE,
+  START_FETCHING_FEATURE_TOGGLE,
+  UPDATE_FEATURE_TOGGLE,
+} from './actions';
+
+export const initialState = {
+  loaded: false,
+  loading: false,
+};
+
+export const featureToggleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case START_FETCHING_FEATURE_TOGGLE:
+      return {
+        ...state,
+        loading: true,
+      };
+    case UPDATE_FEATURE_TOGGLE:
+      return {
+        ...state,
+        ...action.payload,
+        loaded: true,
+        loading: false,
+      };
+    case CLEAR_FEATURE_TOGGLE:
+      return {
+        ...state,
+        loaded: false,
+        loading: false,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
