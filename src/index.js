cb61dcb5c38443e55eaf1f270399b6560fc5f48a
Jakub Gawlikowski <jgawlikowski@ecovadis.com>
2021-05-29 13:57:59 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 export { FeatureToggle } from './FeatureToggle';
 export { On } from './On';
 export { Off } from './Off';
-export { FeatureToggleWrapper } from './FeatureToggleWrapper';
+export {
+  FeatureToggleContext,
+  FeatureToggleWrapper,
+} from './FeatureToggleWrapper';
