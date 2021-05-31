eb30c8108440ad942e298b5a9b91b66bf916a36f
Jakub Gawlikowski <jgawlikowski@ecovadis.com>
2021-05-31 16:16:27 +0200

    adding flagsmith

--- src/Clients/flagsmith.js
+++ src/Clients/flagsmith.js
@@ -1,19 +1,32 @@
 import flagsmith from "flagsmith";
 
-export const flagsmithClient = ({
-  environmentID,
-}) => ({
-  init: handler => {
-    const featureToggles = {};
-    flagsmith.init({
-      environmentID,
-      onChange: (oldFlags, params) => {
-        flagsmith.getFlags()
-          .then(flags => {
-            console.log(flags); // eslint-disable-line
-          });
-        handler(featureToggles);
+class flagsmithClientClass {
+  constructor({ environmentID }) {
+    this.fetching = false;
+    this.environmentID = environmentID;
+    this.init = this.init.bind(this);
+  }
+
+  init(handler) {
+    !this.fetching && flagsmith.init({
+      environmentID: this.environmentID,
+      onChange: () => {
+        const state = flagsmith.getState();
+        handler(this.transform(state));
       }
     });
-  },
+    this.fetching = true;
+  }
+
+  transform(state) {
+    const { flags } = state;
+    const featureFlags = {};
+    Object.keys(flags).forEach(flag => featureFlags[flag] = flags[flag].enabled);
+
+    return featureFlags;
+  }
+}
+
+export const flagsmithClient = ({ environmentID }) => new flagsmithClientClass({
+  environmentID,
 });
