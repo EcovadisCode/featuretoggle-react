ffe3160747ba4545783e6a45eae1dde769402647
Jakub Gawlikowski <jgawlikowski@ecovadis.com>
2021-06-18 16:56:06 +0200

    adding test tasks

---
+++ src/Off.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Off } from './Off';
+
+describe('Off', () => {
+  it('should display child when toggle on', () => {
+    const wrapper = shallow(
+      <Off featureToggleEnabled>
+        <span>Toggle OFF</span>
+      </Off>
+    );
+
+    expect(wrapper
+      .find('span').length)
+      .toEqual(0);
+  });
+
+  it('should not display child when toggle off', () => {
+    const wrapper = shallow(
+      <Off featureToggleEnabled={false}>
+        <span>Toggle OFF</span>
+      </Off>
+    );
+
+    expect(wrapper
+      .find('span')
+      .length)
+      .toEqual(1);
+
+    expect(wrapper
+      .find('span')
+      .text())
+      .toEqual('Toggle OFF');
+  });
+});
